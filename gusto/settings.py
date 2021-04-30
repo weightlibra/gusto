37850ed340fe2b8a6d6825048baf80063eb5fd62
weightlibra <70949450+weightlibra@users.noreply.github.com>
2021-04-30 15:51:00 +0300

    Update settings.py

--- gusto/settings.py
+++ gusto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gusto-apr-30-2021.herokuapp.com']
 
 
 # Application definition
